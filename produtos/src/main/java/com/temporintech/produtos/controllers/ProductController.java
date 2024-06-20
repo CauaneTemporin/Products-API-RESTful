dc6d1d4277a388ec4592cc6c088e02c031efbd45
CauaneTemporin <temporin.cauane@gmail.com>
2024-06-20 20:16:02 -0300

    Criacao do ProductController

---
+++ produtos/src/main/java/com/temporintech/produtos/controllers/ProductController.java
@@ -0,0 +1,13 @@
+package com.temporintech.produtos.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.temporintech.produtos.repositories.ProductRepository;
+
+@RestController
+public class ProductController {
+
+	@Autowired
+	ProductRepository productRepository;
+}
