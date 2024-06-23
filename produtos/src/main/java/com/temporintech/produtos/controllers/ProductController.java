3f3a169593f098b4773458eafccdc2b480ee981f
CauaneTemporin <temporin.cauane@gmail.com>
2024-06-23 19:38:52 -0300

    Adicionando o getAOneProduct

--- produtos/src/main/java/com/temporintech/produtos/controllers/ProductController.java
+++ produtos/src/main/java/com/temporintech/produtos/controllers/ProductController.java
@@ -1,12 +1,15 @@
 package com.temporintech.produtos.controllers;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +36,14 @@ public class ProductController {
 	@GetMapping("/products")
 	public ResponseEntity<List<ProductModel>> getAllProduct() {
 		return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+	}
+
+	@GetMapping("/products/{id}")
+	public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") UUID id) {
+		Optional<ProductModel> product = productRepository.findById(id);
+		if (product.isEmpty())
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+
+		return ResponseEntity.status(HttpStatus.OK).body(product.get());
 	}
 }
