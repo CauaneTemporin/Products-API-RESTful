edb0f21c3da3401e930c03113b6596146fda0d13
CauaneTemporin <temporin.cauane@gmail.com>
2024-06-23 18:34:47 -0300

    Adicionando o saveProduct

--- produtos/src/main/java/com/temporintech/produtos/controllers/ProductController.java
+++ produtos/src/main/java/com/temporintech/produtos/controllers/ProductController.java
@@ -1,13 +1,29 @@
 package com.temporintech.produtos.controllers;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.temporintech.produtos.dtos.ProducRecordtDTO;
+import com.temporintech.produtos.models.ProductModel;
 import com.temporintech.produtos.repositories.ProductRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class ProductController {
 
 	@Autowired
 	ProductRepository productRepository;
+
+	@PostMapping("/products")
+	public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProducRecordtDTO producRecordtDTO) {
+		var productModel = new ProductModel();
+		BeanUtils.copyProperties(producRecordtDTO, productModel);
+		return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+	}
 }
